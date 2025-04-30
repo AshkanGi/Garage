3dc74ed5adb0ef518f02448500ed2a886b55fb64
Ashkan <ashkanghodrati0@gmail.com>
2025-04-30 23:48:29 +0330

    Add Sidebar

--- core/views.py
+++ core/views.py
@@ -5,3 +5,8 @@ from django.views import View
 class Home(View):
     def get(self, request):
         return render(request, 'core/index.html')
+
+
+class Notification(View):
+    def get(self, request):
+        return render(request, 'core/notification.html')
